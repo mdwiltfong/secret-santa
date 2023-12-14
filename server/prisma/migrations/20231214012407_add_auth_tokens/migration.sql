0bfd950acae921694a8ca04aa8052778167f1f12
Grace Kishino <gracekishino@gmail.com>
2023-12-14 10:24:38 +0900

    remove migrations

--- server/prisma/migrations/20231214012407_add_auth_tokens/migration.sql
+++ server/prisma/migrations/20231214012407_add_auth_tokens/migration.sql
@@ -1,26 +1,22 @@
--- CreateEnum
-CREATE TYPE "TokenType" AS ENUM ('EMAIL', 'API');
-
 -- AlterTable
 ALTER TABLE "Users" ALTER COLUMN "firstName" DROP NOT NULL,
 ALTER COLUMN "lastName" DROP NOT NULL;
 
 -- CreateTable
-CREATE TABLE "Token" (
+CREATE TABLE "Tokens" (
     "id" SERIAL NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
-    "type" "TokenType" NOT NULL,
-    "emailToken" TEXT,
+    "jwtToken" TEXT,
     "valid" BOOLEAN NOT NULL DEFAULT true,
     "expiration" TIMESTAMP(3) NOT NULL,
     "userId" INTEGER NOT NULL,
 
-    CONSTRAINT "Token_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "Tokens_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Token_emailToken_key" ON "Token"("emailToken");
+CREATE UNIQUE INDEX "Tokens_jwtToken_key" ON "Tokens"("jwtToken");
 
 -- AddForeignKey
-ALTER TABLE "Token" ADD CONSTRAINT "Token_userId_fkey" FOREIGN KEY ("userId") REFERENCES "Users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Tokens" ADD CONSTRAINT "Tokens_userId_fkey" FOREIGN KEY ("userId") REFERENCES "Users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;

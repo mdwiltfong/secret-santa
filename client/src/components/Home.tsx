2479f9395931cb25194694950ea0fdc050e54169
Ivy Chen <ichen@ucsc.edu>
2023-12-16 14:33:29 +0800

    changed styling and added giftshop page/cards

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -3,12 +3,14 @@ import Navbar from './Navbar'
 function Home() {
     return(
         <>
-        <Navbar path="/organize" title="Organize"/>
+        <Navbar/>
+        <div className="background-morespace">
         <div className="container">
             <div className="christmas-box">
                 <h1 className="box-text text-center ">{25 - new Date().getDate().valueOf()}</h1>  
                 <p className='box-description text-center'>Days until Secret Santa Reveal</p>
             </div> 
+        </div>
         </div>
     </>
   );

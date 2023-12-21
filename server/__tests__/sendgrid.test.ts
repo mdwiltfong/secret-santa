5f23d1049afe6788487771c541dfae90d7ca80bf
Michael Wiltfong <76107997+mdwiltfong@users.noreply.github.com>
2023-12-21 06:37:19 -0500

    sendGrid tests

--- server/__tests__/sendgrid.test.ts
+++ server/__tests__/sendgrid.test.ts
@@ -1,12 +1,19 @@
 import { jest } from "@jest/globals";
 import sendGrid from "../sendGrid/app";
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import SendGrid from "@sendgrid/mail";
-jest.mock("@sendgrid/mail");
-const mockSendGrid = describe("SendGrid Tests", () => {
+jest.mock("@sendgrid/mail", () => {
+  return {
+    setApiKey: jest.fn(),
+    send: jest.fn(() => 202),
+  };
+});
+describe("SendGrid Tests", () => {
   test("SendGrid should be defined", () => {
     expect(sendGrid).toBeDefined();
   });
   test("Sends email", async () => {
-    SendGrid.send.mockResolvedValueOnce();
+    const responseCode = await sendGrid.sendEmail("testToken", "testEmail");
+    expect(responseCode).toBe(202);
   });
 });

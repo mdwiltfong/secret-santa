5e97be16b03d6d9d2b5b690e046afb6e8cfb2b92
Michael Wiltfong <76107997+mdwiltfong@users.noreply.github.com>
2024-01-07 12:09:57 -0500

    #29 FrontEnd tests for PR #22 (#30)
    * Brings in front-end code plus smoke tests
    
    ---------
    
    Co-authored-by: Ivy Chen <ichen@ucsc.edu>

---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,28 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Link } from "react-router-dom";
+
+
+function Navbar(){
+  return (
+    <>
+      <nav className="navbar">
+        <a href="#" className="navbar-brand">
+          Secret Santa
+        </a>
+        <ul className="nav justify-content-end">
+            <Link to="/organize" className="border-underline">
+              Organize
+            </Link>
+            <Link to="/buy" className="border-underline">
+              Buy Gifts
+            </Link>
+            <Link to="/login" className="border-underline">
+              Log in
+            </Link>
+        </ul>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;

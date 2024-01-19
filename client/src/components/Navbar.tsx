059e6680074b88bd561c3e054988a1ea7caa6296
Michael Wiltfong <76107997+mdwiltfong@users.noreply.github.com>
2024-01-18 19:18:30 -0500

    Add login route and update navbar (#45)

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,24 +1,24 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Link } from "react-router-dom";
 
-
-function Navbar(){
+function Navbar() {
   return (
     <>
       <nav className="navbar">
-        <a href="#" className="navbar-brand">
+        <Link to="/" className="navbar-link">
           Secret Santa
-        </a>
+        </Link>
+
         <ul className="nav justify-content-end">
-            <Link to="/organize" className="border-underline">
-              Organize
-            </Link>
-            <Link to="/buy" className="border-underline">
-              Buy Gifts
-            </Link>
-            <Link to="/login" className="border-underline">
-              Log in
-            </Link>
+          <Link to="/organize" className="border-underline">
+            Organize
+          </Link>
+          <Link to="/buy" className="border-underline">
+            Buy Gifts
+          </Link>
+          <Link to="/login" className="border-underline">
+            Log in
+          </Link>
         </ul>
       </nav>
     </>
